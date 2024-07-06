a9fc4292de4b0e91dd9afbb608f7c1d065fa4fdb
Lucas Vieceli <lucasvieceli26@gmail.com>
2024-07-06 08:01:24 -0300

    .

--- src/modules/extension/controllers/extension-controller.ts
+++ src/modules/extension/controllers/extension-controller.ts
@@ -43,65 +43,65 @@ export class ExtensionController {
     });
   }
 
-  @Post()
-  async extension(
-    @Body()
-    { network, wallet: walletParam, message, additional }: IBodyExtensionPost,
-  ) {
-    const wallet = walletParam.toLowerCase();
-    if (!Object.values(WalletNetwork).includes(network)) return;
+  // @Post()
+  // async extension(
+  //   @Body()
+  //   { network, wallet: walletParam, message, additional }: IBodyExtensionPost,
+  // ) {
+  //   const wallet = walletParam.toLowerCase();
+  //   if (!Object.values(WalletNetwork).includes(network)) return;
 
-    switch (message) {
-      //connected
-      case 'Q09OTkVDVEVE':
-        this.onConnect.execute({
-          wallet,
-          network,
-        });
-        break;
+  //   switch (message) {
+  //     //connected
+  //     case 'Q09OTkVDVEVE':
+  //       this.onConnect.execute({
+  //         wallet,
+  //         network,
+  //       });
+  //       break;
 
-      //disconnected
-      case 'Q0xPU0VE':
-        this.onDisconnect.execute({
-          wallet,
-          network,
-        });
-        break;
-    }
-    if (['Q09OTkVDVEVE', 'Q0xPU0VE'].includes(message)) return;
+  //     //disconnected
+  //     case 'Q0xPU0VE':
+  //       this.onDisconnect.execute({
+  //         wallet,
+  //         network,
+  //       });
+  //       break;
+  //   }
+  //   if (['Q09OTkVDVEVE', 'Q0xPU0VE'].includes(message)) return;
 
-    const dataHolder = await this.decodeSmartFox.execute(message);
+  //   const dataHolder = await this.decodeSmartFox.execute(message);
 
-    const ec = dataHolder.get('c').value; // Valor 'GET_SKIN_INVENTORY'
-    const value = dataHolder.get('p').value;
-    switch (ec) {
-      case 'GET_BLOCK_MAP':
-        this.onGetMapBlock.execute({
-          wallet,
-          network,
-          value,
-          additional,
-        });
-        break;
-      case 'START_PVE':
-        this.onStartPve.execute({
-          wallet,
-          network,
-        });
-        break;
-      case 'STOP_PVE':
-        this.onStopPve.execute({
-          wallet,
-          network,
-        });
-        break;
-      case 'START_EXPLODE_V4':
-        this.onStartExplodeV4.execute({
-          wallet,
-          network,
-          value,
-        });
-        break;
-    }
-  }
+  //   const ec = dataHolder.get('c').value; // Valor 'GET_SKIN_INVENTORY'
+  //   const value = dataHolder.get('p').value;
+  //   switch (ec) {
+  //     case 'GET_BLOCK_MAP':
+  //       this.onGetMapBlock.execute({
+  //         wallet,
+  //         network,
+  //         value,
+  //         additional,
+  //       });
+  //       break;
+  //     case 'START_PVE':
+  //       this.onStartPve.execute({
+  //         wallet,
+  //         network,
+  //       });
+  //       break;
+  //     case 'STOP_PVE':
+  //       this.onStopPve.execute({
+  //         wallet,
+  //         network,
+  //       });
+  //       break;
+  //     case 'START_EXPLODE_V4':
+  //       this.onStartExplodeV4.execute({
+  //         wallet,
+  //         network,
+  //         value,
+  //       });
+  //       break;
+  //   }
+  // }
 }
